38967cc1d1b9ff8a863817f7a004c3ce7f8c971c
WilsonGoins <wilson.goins@ufl.edu>
2023-12-02 14:07:18 -0500

    in process of fixing data collection

--- fetch.py
+++ fetch.py
@@ -21,7 +21,6 @@ def getArticles(arr, startYear, endYear):
         for j in range(13):
             year = i
             month = j
-    
 
             url = f'https://api.nytimes.com/svc/archive/v1/{year}/{month}.json?api-key={api_key}'
 
@@ -52,17 +51,23 @@ def getArticles(arr, startYear, endYear):
                         yearPub = time[0:4]
                         monthPub = time[6:7]
 
+
                         # extracting  keywords
-                        keywords = [keyword['value'] for keyword in article_data.get('keywords', [])]
+                        # old (got all keys) keywords = [keyword['value'] for keyword in article_data.get('keywords', [])]
+
+                        # new (gets 1st key)
+                        keywords = [article_data.get('keywords', [])[0]['value']] if article_data.get('keywords') else []
+
 
                         arr.append(Article(title, yearPub, monthPub, url, keywords))
 
-                    for article_info in arr:
-                        print("Title:", article_info.headline)
-                        print("URL:", article_info.url)
-                        print("Date Published:", article_info.year, "-", article_info.month)
-                        print("Keywords:", ", ".join(article_info.keywords))
-                        print("\n" + "=" * 50 + "\n")
+                    # for article_info in arr:
+                    #     print("Title:", article_info.headline)
+                    #     print("URL:", article_info.url)
+                    #     print("Date Published:", article_info.year, "-", article_info.month)
+                    #     print("Keywords:", ", ".join(article_info.keywords))
+                    #     print("\n" + "=" * 50 + "\n")
+
                 else:
                     print("Error: 'response' or 'docs' keys not found in the API response.")
             else:
@@ -72,11 +77,9 @@ def getArticles(arr, startYear, endYear):
             return arr
 
 
-def insertArticles():
-    fileName = 'TrendLens/nyt_data.csv'     # name of file
+def insertArticles(_unorderedMap, _orderedMap):
+    fileName = 'nyt_data.csv'     # name of file
     articleList = []        # list of article objects
-    orderedMap = OrderedMap()               # empty ordered map
-    unorderedMap = unordered_map()          # empty unordered map
 
     with open(fileName, 'r') as file:
         csvReader = csv.reader(file)       # helper to read the csv file from the CSV package
@@ -91,15 +94,25 @@ def insertArticles():
             month = int(row[2])
             url = row[3]
             keyword = row[4]        # just get the first keyword
+
+            # if (month == 2):
+            print("title = ", title, " | keyword = ", keyword, " | year = ", year, " | month = ", month)
+
             # make a new article object
             currentObj = Article(title, year, month, title, keyword)
             # add it to the list
             articleList.append(currentObj)
 
         # insert all articles into the maps
-        for article in articleList:
-            unorderedMap[article.keyword] = article     # put into map
-            orderedMap[article.keyword] = article       # put into map
+        for art in articleList:
+            _unorderedMap[art.keyword] = art     # put into map
+            # _orderedMap[art.keyword] = art       # put into map
 
-        # now print the size of the maps
-        print(unorderedMap.GetSize())
+
+if __name__ == "__main__":
+    orderedMap = OrderedMap()               # empty ordered map
+    unorderedMap = unordered_map()          # empty unordered map
+    insertArticles(unorderedMap, orderedMap)
+
+    for article in unorderedMap["Unites States Politics and Government"]:
+        print(article.title, " -- ", article.year)
