6325c0b9a54e62687b7af4088b3b4d89fe118633
WilsonGoins <wilson.goins@ufl.edu>
2023-12-02 15:38:57 -0500

    finished plotline (for now)

--- app.py
+++ app.py
@@ -9,18 +9,11 @@ from unordered_map import unordered_map
 # get data
 df = pd.read_csv("temp_data.csv")
 
-
 app = Dash(__name__)            # initialize app
-# app layout
-# variables
-    # df = px.data.stocks(index=True, dateTimes=True)
-# fig = px.scatter(df, trendline="rolling", trendline_options=dict(window=5), title="Results for keyword")
-# fig.data = [t for t in fig.data if t.mode == "lines"]
-# fig.update_traces(showlegend=True)
-# fig.show()
 
+# app layout
 app.layout = html.Div([
-    html.Div(children="The Next Facebook v4"),
+    html.Div(children="The Next Facebook v6"),
     # dcc.Graph(figure=fig)
     dcc.Graph(
         id="line-plot",
@@ -29,20 +22,10 @@ app.layout = html.Div([
                           y="Usage",
                           trendline_color_override="blue",
                           title="keyword usage vs year"
-                          ).update_traces(line_shape="linear")
-        # figure=px.histogram(df, x="Year", y="Usage")
+                          ).add_trace(px.line(df, x="Year", y="Usage").data[0])
     )
 ])
 
 # running the app
 if __name__ == "__main__":
-    # info = [["Year", "Usage"], [2000, 40], [2001, 55], [2002, 30], [2003, 29], [2004, 80], [2005, 90]]
-    # csv_file_path = "temp_data.csv"
-    #
-    # # Write data to CSV file
-    # with open(csv_file_path, 'w', newline='') as file:
-    #     writer = csv.writer(file)
-    #     writer.writerows(info)
-
     app.run(debug=True)
-
