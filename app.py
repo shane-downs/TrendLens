7687dcaf55845c84b0f98aba0960cfeb02b6239e
Leonardo D. Cobaleda <116767849+leonardocoba@users.noreply.github.com>
2023-12-02 19:38:04 -0500

    Working updated Line graph with api ( bugs)

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import csv
-from dash import Dash, html, dcc, callback, Output, Input, State
+from dash import Dash, html, dcc, callback, Output, Input, State, no_update
 import pandas as pd
 import plotly.express as px
 import statsmodels.api as sm
-from ordered_map import OrderedMap
-from unordered_map import unordered_map
+from create_maps import *
+from fetch import getArticlesFromMapsAndInsertToCSV
+
 
 # get data
-df = pd.read_csv("temp_data.csv")
+df = pd.read_csv("formatted_nyt_data.csv")
 
 app = Dash(__name__)
 
@@ -28,13 +29,12 @@ app.layout = html.Div([
 ])
 
 
-def update_graph(start_year, end_year, keyword):
+def update_graph(start_year, end_year):
 
     # scanning csv
+
     filtered_df = df[(df['Year'] >= start_year) & (df['Year'] <= end_year)]
     
-    # if keyword:
-    #     filtered_df = filtered_df[filtered_df['Keyword'] == keyword]
 
     fig = px.scatter(filtered_df, x="Year", y="Usage", trendline_color_override="blue", title="keyword usage vs year")
     fig.add_trace(px.line(filtered_df, x="Year", y="Usage").data[0])
@@ -44,19 +44,26 @@ def update_graph(start_year, end_year, keyword):
 @app.callback(
     Output("line-plot", "figure"),
     [Input('submit-val', 'n_clicks')],
-    [Input('start-year-input', 'value'),
-     Input('end-year-input', 'value'),
-     Input('keyword-input', 'value')]
+    [State('start-year-input', 'value'),
+     State('end-year-input', 'value'),
+     State('keyword-input', 'value')]
 )
-def handleSubmit(clicked, start_year, end_year, keyword):
-    if clicked == 0:
-        return Dash.no_update
+def handleSubmit(n_clicks, start_year, end_year, keyword):
+    if n_clicks == 0:
+        return no_update
     else:
-        clicked = 0
+        n_clicks = 0
         if start_year is None or end_year is None or start_year > end_year:
-            return Dash.no_update
-        
-        return update_graph(start_year, end_year, keyword)
+            return no_update
+        print("building")
+        getArticlesFromMapsAndInsertToCSV(keyword, start_year,end_year, nyt_unordered_map,nyt_ordered_map )
+        print("done")
+        return update_graph(start_year, end_year)
 
 if __name__ == "__main__":
+    articles_list = read_csv_to_list()
+    nyt_ordered_map = create_ordered_map(articles_list)
+    nyt_unordered_map = create_unordered_map(articles_list)
     app.run(debug=True)
+
+
